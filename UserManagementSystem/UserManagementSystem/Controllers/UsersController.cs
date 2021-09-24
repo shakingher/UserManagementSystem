e743ec7273ced5c04c36614e6d05c96299b7b3e6
shakingher <davismit.mishra@gmail.com>
2021-09-24 13:32:44 +0530

    Creating the main project file

---
+++ UserManagementSystem/UserManagementSystem/Controllers/UsersController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserManagementSystem.Models;
+
+namespace UserManagementSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            using(var context = new UserDBContext())
+            {
+                return context.Users.ToList();
+            }
+        }
+        [HttpPost]
+        public IActionResult Post([FromBody] User u)
+        {
+            using (var context = new UserDBContext())
+            {
+                if(u == null)
+                {
+                    return BadRequest();
+                }
+                else
+                {
+                    context.Users.Add(u);
+                    context.SaveChanges();
+                    return Ok();
+                }
+            }
+        }
+    }
+}
